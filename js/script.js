ade9be2bdd8f6553fa265ff612412b7aab0b000e
deepakmCollabera <61048374+deepakmCollabera@users.noreply.github.com>
2021-04-20 18:25:06 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $(".modal_open").click(function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    let modal = $(this).attr("data-modalName");
+    $("#"+modal+"").addClass("active");
+  })
+
+  $(".custom_modal_header button").click(function(){
+    $(this).parent().parent().parent(".custom_modal").removeClass("active");
+  })
+
+  // scroll_actions -active
+  $(".scroll_actions li span").click(function(){
+    $(this).parent().parent().parent(".scroll_actions").toggleClass("active");
+  })
+
+  // profile_card_show
+  $(".profile_card_show").click(function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    $(this).children(".profile_card").toggleClass("active");
+  })
+})
